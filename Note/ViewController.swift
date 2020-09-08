e1b7dcf1a282e7bc7b8c712e254f90751b123a66
zero0dayali <officialali35@gmail.com>
2020-09-08 13:18:39 +0700

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Ali on 08/09/20.
+//  Copyright © 2020 Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
